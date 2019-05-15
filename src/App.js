c891f9fe0c1f58c2dbf322f0a03b1246a9828ad4
Matt <walkerjmatt@gmail.com>
2019-05-15 13:34:23 -0700

    updated descriptions

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ import link from './in.png'
 import git from './GitHub-Mark-120px-plus.png'
 import walker from './MattWalkerResume.pdf'
 import neu from './logo.png'
+import email from './email.png'
+import cse from './cse.jpg'
 import './App.css';
 
 // function App() {
@@ -33,8 +35,10 @@ import './App.css';
 // }
 function handleScroll() {
   console.log(document.getElementById('header').className);
-  if (!document.getElementById('header').className.includes('gif')) {
-    //document.getElementById('header').classList.add('App-headergif');
+  if (!document.getElementById('resume').className.includes('show')) {
+    document.getElementById('resume').classList.add('show');
+    document.getElementById('linkedin').classList.add('show');
+    document.getElementById('email').classList.add('show');
   }
   console.log(document.getElementById('header').className);
 
@@ -45,19 +49,19 @@ function App() {
   return (
     <div className="App" onScroll={handleScroll}>
       <div className="NavBar">
-        <div className="Resume">
+        <div id="resume" className="Resume">
           <a href={walker} target ="_blank">
             <img src={resume} alt="resume" width="25" />
           </a>
             </div>
-        <div className="Resume">
+        <div id="linkedin" className="Resume">
           <a href="https://linkedin.com/in/walkerjmatt" target ="_blank">
             <img src={link} alt="link" width="25" />
           </a>
         </div>
-        <div className="Resume">
-          <a href="https://github.com/walkerjmatt" target ="_blank">
-            <img src={git} alt="git" width="25" />
+        <div id="email" className="Resume">
+          <a href="mailto:walkerjmatt@gmail.com" target ="">
+            <img src={email} alt="email" width="25" />
             </a>
         </div>
       </div>
@@ -73,23 +77,36 @@ function App() {
     </div>
       <div className="middleBar">
         <a className="work">Experience</a>
-        &nbsp;<a className="education">& Education</a>
+        <a className="education">, Education</a>
+        <a className="education"> & Projects</a>
       </div>
       <div className="riivos">
         <a className="riivosPic">
           <img className="picture" src={riivos} alt="riivos" />
         </a>
         <div className="riivosDescription">
-          <a className='riivosTitle'>Front End Engineer</a>
-          <div className= 'riivosContent'>August 2018 - April 2019 (Full-time)
-            <li>Implemented and enhance various chart types for improved data visualization</li>
-            <li>Worked closely with product leads to prioritize new features</li>
-            <li>Riivos ceased operations in early April</li>
-            <p></p>
-            June - Sept 2016, June - Sept 2017 (Intern)
-            <li>Added various UI and UX improvements to our SaaS platform</li>
-            <li>Updated and improved code coverage by adding significant unit tests</li>
-            <li>Learned about the inner workings of a startup and how all the various pieces tie together</li>
+          <a className='riivosTitle'>Software Engineer (former intern)</a>
+          <div className= 'riivosContent'>June 2016 - April 2019
+            <li>Added the ability to organize and stack data on charts, increasing the number of data visualizations by 3x</li>
+            <li>Designed and built release notifications that increased usage rates of new features by 60% in their first week</li>
+            <li>Designed and built a proof of concept to incorporate push notifications into chat</li>
+            <li>Implemented mobile compatibility for sidebar navigation</li>
+            <li>Increased code coverage to >80% on automated unit tests</li>
+            <li>Documented on-boarding process to reduce new engineer’s startup time by 5+ hours</li>
+          </div>
+        </div>
+      </div>
+      <div className="riivos">
+        <a className="riivosPic">
+          <img className="picture" src={cse} alt="cse" />
+        </a>
+        <div className="ucsdDescription">
+          <a className='riivosTitle'>Computer Science Tutor</a>
+          <div className= 'riivosContent'>December - June 2017
+            <li>Explained key programming concepts to students</li>
+            <li>Helped multiple students go from below a 2.5 GPA to >3.5 the following quarter</li>
+            <li>Provided real-life examples of concepts to deepen understanding</li>
+            <li>Taught students about time management and project prioritization techniques</li>
           </div>
         </div>
       </div>
@@ -101,9 +118,9 @@ function App() {
           <a className='riivosTitle'>UC San Diego, Jacobs School of Engineering</a>
           <div className= 'riivosContent'>September 2014 - June 2018
             <li>B.S. Computer Science</li>
-            <li>Area Studies in Economics & Mythology</li>
-            <li>Intramural Soccer & Basketball</li>
-            <li>Member of Phi Gamma Delta</li>
+            <li>Concentration in Economics</li>
+            <li>Concentration in Mythology</li>
+            <li>Intramural Basketball & Soccer, Phi Gamma Delta</li>
           </div>
         </div>
       </div>
@@ -113,7 +130,7 @@ function App() {
         </a>
         <div className="neuDescription">
           <a className='riivosTitle'>Neufarms</a>
-          <div className= 'riivosContent'>
+          <div className= 'riivosContent'>Helps provide local & sustainable produce straight to consumers
             <li>Built in Next.JS & React</li>
             <li>Designed and developed branding, UX, and UI for the client</li>
             <li>Worked with Founder to develop a business plan for the company</li>
